5943f631bfac673cd01dd203305a8f88127d7781
repat <repat@repat.de>
2013-06-21 16:11:07 +0200

    slim version with fullscreen, updated README

--- README.md
+++ README.md
@@ -1,29 +1,41 @@
-This is a minimal WebKit based browser.
+This is a minimal WebKit based browser in fullscreen.
 It's intended use is to display a single web resource non-interactively,
 while being highly portable, especially to the arm11 architecture (used for the Raspberry Pi), and supporting Javascript and HTML5 Websockets for dynamic content updates.
+[Peter Schultz](https://github.com/pschultz/kiosk-browser "kiosk-browser on github") wrote the original version. See the changes under changelog.
 
-# COMPILING
+## Compiling
     apt-get install libwebkit-dev
     # OR: yum install webkitgtk-devel
-    make
+    
+    gcc -o browser browser.c $(pkg-config --libs --cflags gtk+-2.0 webkit-1.0)
 
-# INSTALLING
+## Installing
 No special steps are required for installation. Just execute `browser`.
 This program was designed to be started via inittab on boot like this:
 
     1:2345:respawn:/usr/bin/startx -e /usr/bin/browser http://10.0.0.5/zfs/monitor tty1 </dev/tty1 >/dev/tty1 2>&1
 
-# USAGE
-    browser <URL>
+## Usage
+    ./browser
 
-This will launch the browser in fullscreen mode and load the resource at `URL`.
-`URL` can be anything that WebKit supports, including `file://`-URLs for local webpages and documents.
-Omitting `URL` shows the default page, this README.
+This will launch the browser in fullscreen mode and load the resource from`default_url` hardwired in the source.
+`default_url` can be anything that WebKit supports, including `file://`-URLs for local webpages and documents.
 
 The following keybindings exist:
   - `F5` for reloading the current page
-  - `F11` for toggling fullscreen
-
 These actions are also implemented as signal handlers:
   - `HUP` for reload
-  - `USR1` for fullscreen toggle
+
+## Changelog
+* hardwired position(`int HEIGHT` and `int WIDTH`)
+* hardwired URL (`gchar* DEFAULT_URL`), no commandline argument
+* hardwired position (`GTK_WIN_POS_CENTER_ALWAYS`)
+* no toogle fullscreen with F11, thus removed unmaximize() and USR1
+* deleted Makefile, only one command(see above)
+
+## Contact
+* http://repat.de
+* email: repat[at]repat[dot]de
+* XMPP: repat@jabber.ccc.de
+* Twitter: [repat123](https://twitter.com/repat123 "repat123 on twitter")
+* Flattr: [repat](https://flattr.com/profile/repat "repat on flattr")
--- browser.c
+++ browser.c
@@ -1,3 +1,7 @@
+/**
+Original Code by Peter Schultz, http://github.com/pschulktz/kiosk-browser
+Edited by repat<repat[at]repat[dot]de>, March 2013.
+*/
 #include <stdio.h>
 #include <signal.h>
 
@@ -8,18 +12,23 @@
 gboolean on_key_press(GtkWidget*, GdkEventKey*, gpointer);
 
 void reload_browser(int);
-void toggle_fullscreen(int);
-void maximize();
-void unmaximize();
 
 static WebKitWebView* web_view;
 static GtkWidget *window;
-gchar* default_url = "https://github.com/pschultz/kiosk-browser/blob/master/README.md";
+
+// hardwired default page
+gchar* DEFAULT_URL = "http:\/\/repat.de;
+// hardẃired size
+int HEIGHT = 1600;
+int WIDTH = 900;
 
 int main(int argc, char** argv) {
   gtk_init(&argc, &argv);
 
   window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  gtk_window_set_default_size(GTK_WINDOW(window), HEIGHT, WIDTH);
+  // hardwired position GTK_WIN_POS_CENTER_ALWAYS
+  gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER_ALWAYS);
 
   g_signal_connect(window, "key-press-event", G_CALLBACK(on_key_press), NULL);
   g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
@@ -27,18 +36,16 @@ int main(int argc, char** argv) {
   web_view = WEBKIT_WEB_VIEW(webkit_web_view_new());
 
   signal(SIGHUP, reload_browser);
-  signal(SIGUSR1, toggle_fullscreen);
 
   gtk_container_add(GTK_CONTAINER(window), GTK_WIDGET(web_view));
 
-  if(argc > 1) {
-    webkit_web_view_load_uri(web_view, argv[1]);
-  }
-  else {
-    webkit_web_view_load_uri(web_view, default_url);
-  }
+  webkit_web_view_load_uri(web_view, DEFAULT_URL);
+  
+  // maximize()
+  gtk_window_maximize(GTK_WINDOW(window));
+  gtk_window_fullscreen(GTK_WINDOW(window));
+  gtk_window_set_decorated(GTK_WINDOW(window), FALSE);
 
-  maximize();
   gtk_widget_show_all(window);
   gtk_main();
 
@@ -49,10 +56,6 @@ gboolean on_key_press(GtkWidget* window, GdkEventKey* key, gpointer userdata) {
   if(key->type == GDK_KEY_PRESS && key->keyval == GDK_F5) {
     reload_browser(0);
   }
-  else if(key->type == GDK_KEY_PRESS && key->keyval == GDK_F11) {
-    toggle_fullscreen(0);
-  }
-
   return FALSE;
 }
 
@@ -60,24 +63,3 @@ void reload_browser(int signum) {
   webkit_web_view_reload_bypass_cache(web_view);
 }
 
-void toggle_fullscreen(int signum) {
-  if(gtk_window_get_decorated(GTK_WINDOW(window))) {
-    maximize();
-  }
-  else {
-    unmaximize();
-  }
-}
-
-void maximize() {
-  gtk_window_maximize(GTK_WINDOW(window));
-  gtk_window_fullscreen(GTK_WINDOW(window));
-  gtk_window_set_decorated(GTK_WINDOW(window), FALSE);
-}
-
-void unmaximize() {
-  gtk_window_unmaximize(GTK_WINDOW(window));
-  gtk_window_unfullscreen(GTK_WINDOW(window));
-  gtk_window_set_decorated(GTK_WINDOW(window), TRUE);
-  gtk_window_resize(GTK_WINDOW(window), 1280, 768);
-}
